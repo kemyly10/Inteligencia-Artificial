b75afeaed332b5296f48dffb5f592b7bafbab9c4
kemyly10 <114074834+kemyly10@users.noreply.github.com>
2024-10-28 12:48:47 +0000

    28/10

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal= document.queryselector(".caixa__principal");
+const caixaPerguntas= document.querySelector(".caixa__perguntas");
+const caixaAlternativas= document.querySelector(".caixa__alternativas");
+const caixaResultados= document.queryselector("caixa__resultados");
+const textoResultado= document.queryselector(".texto__resultado");
