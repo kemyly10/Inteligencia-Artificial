68a97b36b8aa83804643d98c4b380e41af52768d
kemyly10 <114074834+kemyly10@users.noreply.github.com>
2024-11-04 11:58:45 +0000

    criacao de perguntas

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const caixaPerguntas= document.querySelector(".caixa__perguntas");
 const caixaAlternativas= document.querySelector(".caixa__alternativas");
 const caixaResultados= document.queryselector("caixa__resultados");
 const textoResultado= document.queryselector(".texto__resultado");
+
+const perguntas=[ 
+    {
+    enunciado: "Pergunta1"
+    alternativas: [
+        "Alternativa 1";
+ "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta1"
+    alternativas: [
+        "Alternativa 1";
+ "Alternativa 2"
+    ]
+  },
+];
+
